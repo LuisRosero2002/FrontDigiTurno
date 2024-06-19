fb4019b5df852abd55d3561d359339c77750bab7
JulianPaz <julianpaz@udenar.edu.co>
2024-06-18 23:50:08 -0500

    Feat: se agregan alertas

--- src/app/modules/turnos/turnos-empleado/turnos-empleado.component.ts
+++ src/app/modules/turnos/turnos-empleado/turnos-empleado.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { ServicioService } from '../../servicios/services/servicio.service';
 import { Turno } from 'src/app/core/models/turno.model';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-turnos-empleado',
@@ -9,7 +10,6 @@ import { Turno } from 'src/app/core/models/turno.model';
   styleUrls: ['./turnos-empleado.component.sass']
 })
 export class TurnosEmpleadoComponent implements OnInit {
-
 
   idEmpleado: number
   dataTurnos: Turno = {
@@ -19,60 +19,100 @@ export class TurnosEmpleadoComponent implements OnInit {
     ESTADO_DESCRIPCION: null,
     FECHA: null,
     LUGAR: null,
-    CLIENTE:null
-  }
+    CLIENTE: null
+  };
+
   constructor(
     private route: ActivatedRoute,
     private servicioService: ServicioService
-
   ) { }
 
   ngOnInit(): void {
-
     this.route.paramMap.subscribe(params => {
       this.idEmpleado = parseInt(params.get('id'));
+      this.mostrarAlerta();
+
     });
-    this.siguinteTurno()
+    this.siguinteTurno();
   }
 
+  mostrarAlerta() {
+    Swal.fire({
+      title: 'Bienvenido',
+      text: 'Has accedido a la vista de turnos del empleado' + this.idEmpleado,
+      icon: 'info'
+    });
+  }
 
   siguinteTurno() {
     this.servicioService.siguienTurno(this.idEmpleado).subscribe(res => {
       if ([res].length === 0) {
-        // ALERTA YA NO HAY MAS TURNOS PARA ESE EMPLEADO
-        console.log("ALERTA YA NO HAY MAS TURNOS PARA ESE EMPLEADO");
+        Swal.fire({
+          title: `¡Buen trabajo, ${this.dataTurnos.NOMBRE_EMPLEADO}!`,
+          text: "No hay más turnos",
+          icon: "success"
+        });
       } else {
-        this.dataTurnos = res
-
+        this.dataTurnos = res;
         this.servicioService.getTurnos().subscribe(res2 => {
           this.servicioService.enviarNuevoTurno(res2);
-        })
+        }, error => {
+          console.error(error);
+          Swal.fire({
+            title: "Error",
+            text: "Ocurrió un error al obtener los turnos",
+            icon: "error"
+          });
+        });
         console.log(this.dataTurnos);
-
       }
-    })
+    }, error => {
+      console.error(error);
+      Swal.fire({
+        title: "Error",
+        text: "Ocurrió un error al obtener el siguiente turno",
+        icon: "error"
+      });
+    });
   }
 
   finTurno() {
-    debugger
-    const dataInput =  {ID:this.dataTurnos['ID']}
-    if(dataInput){
+    const dataInput = { ID: this.dataTurnos['ID'] };
+    if (dataInput) {
       this.servicioService.finTurno(dataInput).subscribe(res => {
-        if ([res].length === 0) {
-          // ALERTA YA NO HAY MAS TURNOS PARA ESE EMPLEADO
-          console.log("ALERTA YA NO HAY MAS TURNOS PARA ESE EMPLEADO");
+        if (res.length === 0) {
+          Swal.fire({
+            title: `¡Buen trabajo, ${this.dataTurnos.NOMBRE_EMPLEADO}!`,
+            text: "No hay más turnos",
+            icon: "success"
+          });
         } else {
-  
           this.servicioService.getTurnos().subscribe(res2 => {
             this.servicioService.enviarNuevoTurno(res2);
-          })
+          }, error => {
+            console.error(error);
+            Swal.fire({
+              title: "Error",
+              text: "Ocurrió un error al obtener los turnos",
+              icon: "error"
+            });
+          });
           console.log(this.dataTurnos);
-  
         }
-      })
-    }else{
-      console.log("ALERTA YA NO HAY TURNO ACTUAL");
+      }, error => {
+        console.error(error);
+        Swal.fire({
+          title: "Error",
+          text: "Ocurrió un error al finalizar el turno",
+          icon: "error"
+        });
+      });
+    } else {
+      Swal.fire({
+        title: `¡Buen trabajo, ${this.dataTurnos.NOMBRE_EMPLEADO}!`,
+        text: "No hay más turnos",
+        icon: "success"
+      });
     }
-
   }
 }
